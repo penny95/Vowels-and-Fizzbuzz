db33c4d377287d7fff01bdcb060dc051d467fad3
penny95 <49157799+penny95@users.noreply.github.com>
2019-11-29 23:31:47 +0300

    changed return statements to as in the test file

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,16 @@ function fizzBuzz(list1, list2) {
 var sumOfArrayLengths=list1.length + list2.length;
     
     if(sumOfArrayLengths%3 == 0 && sumOfArrayLengths%5!== 0 ){
-        return "fizz";
+        return "Fizz";
     }
     else if(sumOfArrayLengths%5 == 0 && sumOfArrayLengths%3!== 0){
         return "Buzz";
     }
     else if(sumOfArrayLengths %3 == 0 && sumOfArrayLengths%5== 0 ){
-        return "fizzBuzz" ;
+        return "Fizzbuzz" ;
     }
     else if(sumOfArrayLengths%3!== 0 && sumOfArrayLengths%5 !== 0){
         return sumOfArrayLengths ;
     }
 }
-console.log(fizzBuzz([1, 2, 3,4,5,6,7,8], [1, 2, 3,4,5,6,7]));
 module.exports = fizzBuzz;
